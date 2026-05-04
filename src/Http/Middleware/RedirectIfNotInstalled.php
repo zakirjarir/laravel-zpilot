c92fb1493b07dcb9d1e8a21a5c9d29c918bb507c
zakirjarir <zakirjarir@gmail.com>
2026-05-04 08:43:42 +0600

    chore: release v1.5.1

--- src/Http/Middleware/RedirectIfNotInstalled.php
+++ src/Http/Middleware/RedirectIfNotInstalled.php
@@ -10,9 +10,6 @@ class RedirectIfNotInstalled
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
